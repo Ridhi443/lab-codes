75f7b0985442210db5616803c03912483174c1b8
Ridhi443 <ridhigulati156@gmail.com>
2025-12-14 15:46:45 +0530

    Create extra lab codes10.c
    This program prompts the user to enter two arrays and then finds and prints the common elements between them.

---
+++ extra lab codes10.c
@@ -0,0 +1,27 @@
+// write a program to find the common elements between two arrays.
+#include <stdio.h>
+
+int main() {
+    int a[10], b[10], n1, n2;
+
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
+    for (int i = 0; i < n1; i++)
+        scanf("%d", &a[i]);
+
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
+    for (int i = 0; i < n2; i++)
+        scanf("%d", &b[i]);
+
+    printf("Common elements:\n");
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n2; j++) {
+            if (a[i] == b[j]) {
+                printf("%d ", a[i]);
+                break;
+            }
+        }
+    }
+    return 0;
+}
