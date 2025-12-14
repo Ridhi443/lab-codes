f26efdd8213b94100e6680e7dc71da45b3a53258
Ridhi443 <ridhigulati156@gmail.com>
2025-12-14 16:09:12 +0530

    Create extra lab code17.c
    This program rotates an N×N matrix clockwise by 90 degrees in place using transposition and row reversal.

---
+++ extra lab code17.c
@@ -0,0 +1,42 @@
+/* Rotate an N×N matrix clockwise by 90 degrees in place.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int a[n][n];
+
+    printf("Enter matrix:\n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &a[i][j]);
+
+    // Transpose
+    for (int i = 0; i < n; i++)
+        for (int j = i; j < n; j++) {
+            int temp = a[i][j];
+            a[i][j] = a[j][i];
+            a[j][i] = temp;
+        }
+
+    // Reverse rows
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n / 2; j++) {
+            int temp = a[i][j];
+            a[i][j] = a[i][n - j - 1];
+            a[i][n - j - 1] = temp;
+        }
+
+    printf("Rotated Matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
+            printf("%d ", a[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
